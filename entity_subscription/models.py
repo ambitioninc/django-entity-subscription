52733e4e35cea7b41249e13bede36bf5ef62bb53
Erik Swanson <theerikswanson@gmail.com>
2014-06-06 12:23:41 -0400

    Model doc-strings.

--- entity_subscription/models.py
+++ entity_subscription/models.py
@@ -4,6 +4,13 @@ from entity.models import Entity
 
 
 class Subscription(models.Model):
+    """Include groups of entities to subscriptions.
+
+    It is recommended that these be largely pre-configured within an
+    application, as catch-all groups. The finer grained control of
+    individual users subscription status is defined within the
+    `Unsubscribe` model.
+    """
     medium = models.ForeignKey('Medium')
     source = models.ForeignKey('Source')
     entity = models.ForeignKey(Entity)
@@ -11,18 +18,37 @@ class Subscription(models.Model):
 
 
 class Unsubscribe(models.Model):
+    """Individual entity-level unsubscriptions.
+
+    Entities can opt-out individually from recieving any notification
+    of a given source/medium combination.
+    """
     entity = models.ForeignKey(Entity)
     medium = models.ForeignKey('Medium')
     source = models.ForeignKey('Source')
 
 
 class Medium(models.Model):
+    """A method of actually delivering the notification to users.
+
+    Mediums describe a particular method the application has of
+    sending notifications. The code that handles actually sending the
+    message should own a medium object that represents itself, or at
+    least, know the name of one.
+    """
     name = models.CharField(max_length=64, unique=True)
     display_name = models.CharField(max_length=64)
     description = models.TextField()
 
 
 class Source(models.Model):
+    """A category of where notifications originate from.
+
+    Sources should make sense as a category of notifications to users,
+    and pieces of the application which create that type of
+    notification should own a `source` object which they can pass
+    along to the business logic for distributing the notificaiton.
+    """
     name = models.CharField(max_length=64, unique=True)
     display_name = models.CharField(max_length=64)
     description = models.TextField()
