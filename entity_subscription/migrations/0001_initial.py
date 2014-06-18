3d6586317c39f1476aa6ffebca36c334994ea3f7
Erik Swanson <theerikswanson@gmail.com>
2014-06-18 11:22:56 -0400

    Add migrations, bump version number.

---
+++ entity_subscription/migrations/0001_initial.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Subscription'
+        db.create_table(u'entity_subscription_subscription', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('medium', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity_subscription.Medium'])),
+            ('source', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity_subscription.Source'])),
+            ('entity', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity.Entity'])),
+            ('subentity_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], null=True)),
+        ))
+        db.send_create_signal(u'entity_subscription', ['Subscription'])
+
+        # Adding model 'Unsubscribe'
+        db.create_table(u'entity_subscription_unsubscribe', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('entity', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity.Entity'])),
+            ('medium', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity_subscription.Medium'])),
+            ('source', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['entity_subscription.Source'])),
+        ))
+        db.send_create_signal(u'entity_subscription', ['Unsubscribe'])
+
+        # Adding model 'Medium'
+        db.create_table(u'entity_subscription_medium', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=64)),
+            ('display_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'entity_subscription', ['Medium'])
+
+        # Adding model 'Source'
+        db.create_table(u'entity_subscription_source', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=64)),
+            ('display_name', self.gf('django.db.models.fields.CharField')(max_length=64)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+        ))
+        db.send_create_signal(u'entity_subscription', ['Source'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Subscription'
+        db.delete_table(u'entity_subscription_subscription')
+
+        # Deleting model 'Unsubscribe'
+        db.delete_table(u'entity_subscription_unsubscribe')
+
+        # Deleting model 'Medium'
+        db.delete_table(u'entity_subscription_medium')
+
+        # Deleting model 'Source'
+        db.delete_table(u'entity_subscription_source')
+
+
+    models = {
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'entity.entity': {
+            'Meta': {'object_name': 'Entity'},
+            'entity_id': ('django.db.models.fields.IntegerField', [], {}),
+            'entity_meta': ('jsonfield.fields.JSONField', [], {'null': 'True'}),
+            'entity_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        u'entity_subscription.medium': {
+            'Meta': {'object_name': 'Medium'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        u'entity_subscription.source': {
+            'Meta': {'object_name': 'Source'},
+            'description': ('django.db.models.fields.TextField', [], {}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '64'})
+        },
+        u'entity_subscription.subscription': {
+            'Meta': {'object_name': 'Subscription'},
+            'entity': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity.Entity']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'medium': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity_subscription.Medium']"}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity_subscription.Source']"}),
+            'subentity_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']", 'null': 'True'})
+        },
+        u'entity_subscription.unsubscribe': {
+            'Meta': {'object_name': 'Unsubscribe'},
+            'entity': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity.Entity']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'medium': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity_subscription.Medium']"}),
+            'source': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['entity_subscription.Source']"})
+        }
+    }
+
+    complete_apps = ['entity_subscription']
